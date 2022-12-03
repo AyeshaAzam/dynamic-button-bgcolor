03b4b38401ec0e1f04b9a00cc967f228e5843c61
azamay <ayesha.azam@delaval.com>
2022-12-03 20:25:01 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,78 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Dynamic Button Component" />
+
+    <DynamicButton
+      buttonText="Dark Mode"
+      :dark="true"
+      @click="handleDarkMode"
+      :color="active ? 'red' : 'blue'"
+    />
+    <!-- default is the light mode so we dont have to pass any pops to it-->
+    <DynamicButton
+      buttonText="Light Mode"
+      @click="handleLightMode"
+      :color="active ? this.color : '#16a085'"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import DynamicButton from "./components/DynamicButton.vue";
+
+/**************************************************************************************
+ * Watch: Vue.js - Dynamic & Reusable Button Component In Vue.js
+ * https://www.youtube.com/watch?v=2Evw5h_1Ko8
+ *
+ * Render class bases on PROPS , dynamic class on button by binding class and pass an array
+ *
+ * Using the $listeners property, you can forward all event listeners on
+ * the component to a specific child element with v-on="$listeners"
+ *
+ * check issues I had : https://stackoverflow.com/questions/74643295/button-background-color-doesnot-changes-when-clicked-on-it/74643605?noredirect=1#comment131784986_74643605
+ ***************************************************************************************/
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    DynamicButton,
+  },
+
+  props: {
+    // darkColorChange: {
+    //   type: String,
+    //   default: "",
+    // },
+    // lightColorChange: {
+    //   type: String,
+    //   default: "",
+    // },
+  },
+
+  data() {
+    return {
+      active: true,
+      color: "#3aa1b6",
+    };
+  },
+
+  methods: {
+    handleDarkMode() {
+      console.log("Dark-mode clicked");
+      // eslint-disable-next-line
+      // this.darkColorChange.style.backgroundColor = "pink";
+      this.active = !this.active;
+    },
+
+    handleLightMode() {
+      console.log("Light-mode clicked");
+      this.active = !this.active;
+    },
+  },
+};
 </script>
 
 <style>
---
+++ src/components/DynamicButton.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <div class="btn1">
+      <button
+        v-on="$listeners"
+        :class="[dark ? 'dark' : 'light', 'baseButton']"
+        class="btn"
+        :style="{ backgroundColor: color }"
+      >
+        {{ buttonText }}
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DynamicButton",
+
+  props: {
+    buttonText: {
+      type: String,
+      default: "label",
+    },
+    dark: {
+      type: Boolean,
+      default: false,
+    },
+
+    light: {
+      type: Boolean,
+      default: true,
+    },
+    active: {
+      type: Boolean,
+      default: false,
+    },
+    color: {
+      type: String,
+      default: "gray",
+    },
+  },
+};
+</script>
+
+<style scoped>
+.baseButton {
+  border-radius: 5px;
+  border: none;
+  padding: 10px;
+  width: 200px;
+  height: 30px;
+}
+
+.light {
+  background: white;
+  color: black;
+  border: 1px solid lightgray;
+}
+
+.dark {
+  background: black;
+  color: white;
+}
+
+.btn {
+  margin: 10px;
+}
+</style>
